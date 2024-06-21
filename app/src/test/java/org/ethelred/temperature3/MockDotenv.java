fdb16402b0da36fd2d4c68a8be033d3734a95285
Edward Harman <jaq+github@ethelred.org>
2024-06-21 16:39:57 -0400

    mock environment for test

---
+++ app/src/test/java/org/ethelred/temperature3/MockDotenv.java
@@ -0,0 +1,39 @@
+package org.ethelred.temperature3;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class MockDotenv implements Dotenv {
+    private final Map<String, DotenvEntry> map;
+
+    public MockDotenv(Map<String, String> values) {
+        map = values.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> new DotenvEntry(e.getKey(), e.getValue())));
+    }
+
+    @Override
+    public Set<DotenvEntry> entries() {
+        return Set.copyOf(map.values());
+    }
+
+    @Override
+    public Set<DotenvEntry> entries(Filter filter) {
+        return entries();
+    }
+
+    @Override
+    public String get(String key) {
+        return get(key, null);
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        if (map.containsKey(key)) {
+            return map.get(key).getValue();
+        }
+        return defaultValue;
+    }
+}
--- app/src/test/java/org/ethelred/temperature3/TestFactory.java
+++ app/src/test/java/org/ethelred/temperature3/TestFactory.java
@@ -4,7 +4,12 @@ import io.avaje.inject.Bean;
 import io.avaje.inject.Factory;
 import io.avaje.inject.test.TestScope;
 import javax.sql.DataSource;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.inject.Singleton;
 import org.h2.jdbcx.JdbcDataSource;
+
+import java.util.Map;
 
 @TestScope
 @Factory
@@ -14,5 +19,15 @@ public class TestFactory {
         var ds = new JdbcDataSource();
         ds.setURL("jdbc:h2:mem:test;MODE=MariaDB;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1");
         return ds;
+    }
+
+    @Singleton
+    @Bean
+    public Dotenv testEnv() {
+        return new MockDotenv(Map.of(
+                "OPENWEATHER_ID", "12345",
+                "LAT", "12.3",
+                "LON", "4.56"
+        ));
     }
 }
